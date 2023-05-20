1b80f7adc68ff1c92dfd2c3b15b4a3feb37de1a4
Rene Vogt <rvogt@aki-gmbh.com>
2023-05-20 16:18:55 +0200

    export parameters for the name generation, settings up C# client project that consumes these parameters

---
+++ src/Clients/ML.Tests/Program.cs
@@ -0,0 +1,6 @@
+﻿using NameGeneration;
+
+do
+{
+    Console.WriteLine(NameGenerator.GetName());
+} while (Console.ReadKey(true).Key != ConsoleKey.Escape);
---
+++ src/Clients/NameGeneration/NameGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace NameGeneration;
+
+public static class NameGenerator
+{
+    static NameGenerator()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        using var stream = assembly.GetManifestResourceStream("NameGeneration.names.dat")!;
+        using var memoryStream = new MemoryStream();
+        stream.CopyTo(memoryStream);
+        var array = memoryStream.ToArray();
+        Console.WriteLine(array.Length);
+    }
+
+    public static IEnumerable<string> GetNames()
+    {
+        while (true) yield return GetName();
+    }
+    public static string GetName() => "not yet implemented!";
+}
