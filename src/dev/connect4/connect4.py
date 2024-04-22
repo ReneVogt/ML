602819655e97e56df3ff69738c6d0f4f9a9e6b92
Rene Vogt <rvogt@aki-gmbh.com>
2024-04-22 14:05:12 +0200

    setup connect4 environment with cnn

---
+++ src/dev/connect4/connect4.py
@@ -0,0 +1,119 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import random
+
+class Connect4:   
+    def __init__(self):
+        self.board = torch.zeros(3, 6, 7, dtype = torch.float32)
+        self.board[0, :, :] = 1
+        self.heights = [0, 0, 0, 0, 0, 0, 0]
+        self.player = 1
+        self.opponent = 2
+        self.winner = 0
+        self.full = False
+        self.history = []
+
+    def is_valid(self, action):
+        return action >= 0 and action < 7 and self.heights[action] < 6
+    def charAt(self, col, row):
+        return ' ' if row < 0 or row > 5 or col < 0 or col > 6 or self.board[0, row, col] == 1 else 'X' if self.board[1, row, col] == 1 else 'O'
+    
+    def setAt(self, col):
+        row = self.heights[col]
+        self.board[0, row, col] = 0
+        self.board[self.player, row, col] = 1
+        self.heights[col] += 1
+
+    def move(self, action):
+        if not self.is_valid(action):
+            raise ValueError(f'Column {action} is already filled.')
+        if self.winner != 0:
+            raise ValueError(f'Game is already finished.')
+
+        self.setAt(action)
+
+        row = self.heights[action] - 1
+        pattern = ''.join(['X' if self.player == 1 else 'O'] * 4)
+
+        # check horizontal win
+        if pattern in ''.join([self.charAt(col, row) for col in range(7)]):
+            self.winner = self.player
+        # check vertical win
+        elif pattern in ''.join(self.charAt(action, r) for r in range(self.heights[action])):
+            self.winner = self.player
+        else:
+            # check diagonal win ll to ur
+            indices = [(action + (x - 4), row + (x - 4)) for x in range(7)]
+            diagonal = ''.join([self.charAt(col, row) for col, row in indices])
+            if pattern in diagonal:
+                self.winner = self.player
+            else:
+                # check diagonal win ul to lr
+                indices = [(action + (x - 3), row - (x - 3)) for x in range(7)]
+                diagonal = ''.join([self.charAt(col, row) for col, row in indices])
+                if pattern in diagonal:
+                    self.winner = self.player
+
+        self.player, self.opponent = self.opponent, self.player
+        self.history.append(action)
+        self.full = all(self.heights[i] == 6 for i in range(7))
+
+    @property
+    def state(self): 
+        return self.board.clone()
+
+    def render(self):
+        for row in reversed(range(6)):
+            signs = [self.charAt(col, row) for col in range(7)]
+            print(f"|{''.join(signs)}|")
+
+@torch.no_grad()
+def validate(model : nn.Module, games):
+    train = model.training
+    model.eval()
+
+    crosswins = 0
+    crossdraws = 0
+    crosslosses = 0
+    circlewins = 0
+    circledraws = 0
+    circlelosses = 0
+    qplayer = 2
+
+    for _ in range(games):
+        done = False
+        qplayer = 3 - qplayer
+        env = Connect4()
+        while not done:
+            if qplayer == env.player or random.uniform(0, 1) < 0.5:
+                q = model(env.state)
+                action = max([a for a in range(7) if env.is_valid(a)], key = lambda x: q[x])
+            else:
+                action = random.choice([a for a in range(7) if env.is_valid(a)])
+
+            env.move(action)
+
+            if env.winner != 0:
+                if env.winner == qplayer:
+                    if qplayer == 1:
+                        crosswins += 1
+                    else:
+                        circlewins += 1
+                else:
+                    if qplayer == 1:
+                        crosslosses += 1
+                    else:
+                        circlelosses += 1
+                done = True
+            elif env.full:
+                if qplayer == 1:
+                    crossdraws += 1
+                else:
+                    circledraws += 1
+                done = True
+
+    if train:
+        model.train()
+
+    return crosswins, crossdraws, crosslosses, circlewins, circledraws, circlelosses
--- src/use/Connect4/MoveGenerator.cs
+++ src/use/Connect4/MoveGenerator.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.ML.OnnxRuntime;
 using Microsoft.ML.OnnxRuntime.Tensors;
-using System;
 using System.Reflection;
 
 namespace Connect4;
@@ -19,13 +18,13 @@ static class MoveGenerator
     }
     public static int GetMove(List<int>[] state)
     {
-        var inputTensor = new DenseTensor<float>(new[] { 3*6*7 });
+        var inputTensor = new DenseTensor<float>([3, 6, 7]);
         for (var col = 0; col < 7; col++)
             for (var row = 0; row < 6; row++)
             {
-                var r = 5 - row;
-                var player = state[col].Count > r ? state[col][r] : 0;
-                inputTensor[3*(col * 6 + row) + player] = 1;
+                inputTensor[0, row, col] = state[col].Count <= row || state[col][row] == 0 ? 1 : 0;
+                inputTensor[1, row, col] = state[col].Count > row && state[col][row] == 1 ? 1 : 0;
+                inputTensor[2, row, col] = state[col].Count > row && state[col][row] == 2 ? 1 : 0;
             }
 
         var inputs = new NamedOnnxValue[] { NamedOnnxValue.CreateFromTensor(_model.InputNames[0], inputTensor) };
