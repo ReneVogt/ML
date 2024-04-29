5e3eb0931b6b3e8e47d8ff8ceffb0aa62bc7055e
Rene Vogt <rvogt@aki-gmbh.com>
2024-04-29 12:08:40 +0200

    added user/user mode for testing environment

--- src/use/Connect4/Program.cs
+++ src/use/Connect4/Program.cs
@@ -8,23 +8,34 @@ Console.Clear();
 Write(0, 0, ConsoleColor.Gray, "[ 1 ]:");
 Write(7, 0, ConsoleColor.Red, "USER");
 Write(12, 0, ConsoleColor.Gray, "-");
-Write(15, 0, ConsoleColor.Yellow, "AI");
+Write(15, 0, ConsoleColor.Yellow, "USER");
 Write(0, 1, ConsoleColor.Gray, "[ 2 ]:");
-Write(8, 1, ConsoleColor.Red, "AI");
+Write(7, 1, ConsoleColor.Red, "USER");
 Write(12, 1, ConsoleColor.Gray, "-");
-Write(14, 1, ConsoleColor.Yellow, "USER");
+Write(15, 1, ConsoleColor.Yellow, "AI");
 Write(0, 2, ConsoleColor.Gray, "[ 3 ]:");
 Write(8, 2, ConsoleColor.Red, "AI");
 Write(12, 2, ConsoleColor.Gray, "-");
-Write(15, 2, ConsoleColor.Yellow, "AI");
+Write(14, 2, ConsoleColor.Yellow, "USER");
+Write(0, 3, ConsoleColor.Gray, "[ 4 ]:");
+Write(8, 3, ConsoleColor.Red, "AI");
+Write(12, 3, ConsoleColor.Gray, "-");
+Write(15, 3, ConsoleColor.Yellow, "AI");
 
 bool redHuman = false, yellowHuman = false;
-switch(GetKey(ConsoleKey.D1, ConsoleKey.D2, ConsoleKey.D3, ConsoleKey.Escape))
+switch(GetKey(ConsoleKey.D1, ConsoleKey.D2, ConsoleKey.D3, ConsoleKey.D4, ConsoleKey.Escape))
 {
     case ConsoleKey.D1:
         redHuman = true;
+        yellowHuman = true;
         break;
     case ConsoleKey.D2:
+        redHuman = true;
+        break;
+    case ConsoleKey.D3:
+        redHuman = true;
+        break;
+    case ConsoleKey.D4:
         yellowHuman = true;
         break;
     case ConsoleKey.Escape:
