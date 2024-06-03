81349579b1fdf0731065c1fe2abd1b04904432c0
René Vogt <rene.vogt@gmx.de>
2024-06-03 13:47:38 +0200

    validation against mcts

--- src/dev/connect4/mcts.py
+++ src/dev/connect4/mcts.py
@@ -1,41 +1,50 @@
 import numpy as np
 import math
+import threading
 from board import Connect4Board
 
 class Connect4MCTS():
     _explorationParameter = math.sqrt(2)
-    def __init__(self, strength : int) -> None:
-        if strength < 1:
-            raise ValueError('The strength must be at least 1.')
-        self._strength = strength
-        self._states = {} # state -> ([count]*7, [wins]*7, validMoves)
+    def __init__(self, games : int) -> None:
+        if games < 1:
+            raise ValueError('The number of games must be at least 1.')
+        self._games = games
+
+        # The _states dictioary maps states (Connect4Board.stateKey) to tuples
+        # that contain this information:
+        # - counts : list[int] 
+        #       The counts how often an action was chosen.
+        #       We keep track of the total count in counts[7] so we
+        #       don't need to sum() every time.
+        # - wins : list[int]
+        #       The number of wins that were reached through each action.
+        # - validMoves : list[int]
+        #       The list of valid actions for the given state, so we don't need
+        #       to check every time.
+        # - unplayedMoves : list[int]
+        #       The list of not yet played moves we need to choose randomly from,
+        #       so we don't need to recollect them every time.
+        self._states = {} # state -> ([count]*8, [wins]*7, validMoves, unplayedMoves)
 
     def getMove(self, env : Connect4Board) -> int:      
-        for i in range(self._strength):
+        for i in range(self._games):
             self._evaluate(env.clone())
 
-        (counts, _, validMoves) = self._getEntry(env)
+        (counts, _, validMoves, _) = self._getEntry(env)
         return max(validMoves, key=lambda x: counts[x])
     
     def _evaluate(self, env : Connect4Board) -> int:
-        (counts,wins,validMoves) = self._getEntry(env)
-        unplayedMoves = [a for a in validMoves if counts[a] == 0]
+        (counts,wins,validMoves,unplayedMoves) = self._getEntry(env)
         if len(unplayedMoves) > 0:
             action = np.random.choice(unplayedMoves)
+            unplayedMoves.remove(action)
         else:
-            parentCountLog = math.log(sum(counts))
+            parentCountLog = math.log(counts[7])
             action = max(validMoves, key=lambda x: wins[x]/counts[x] + Connect4MCTS._explorationParameter * math.sqrt(parentCountLog/counts[x]))
 
+        counts[7] += 1
         counts[action] += 1
-        previousHeights = [env._getColumnHeight(a) for a in range(7)]
-        previousEnv = env.clone()
         env.move(action)
-        currentHeights = [env._getColumnHeight(a) for a in range(7)]
-        if any(currentHeights[a] - previousHeights[a] > 1 for a in range(7)):
-            print(f'INVALID FOLLOW UP STATE')
-            print(f'{previousEnv.stateKey} {[previousEnv._getColumnHeight(a) for a in range(7)]} {action}')            
-            print(f'{env.stateKey} {[env._getColumnHeight(a) for a in range(7)]}')
-            raise MCTSException(previousEnv, env)
 
         if env.Finished:
             result = 1 if env.Winner != Connect4Board.EMPTY else 0.5
@@ -51,7 +60,8 @@ class Connect4MCTS():
     def _getEntry(self, env : Connect4Board):
         entry = self._states.get(env.stateKey)
         if entry is None:
-            entry = ([0] * 7, [0] * 7, [a for a in range(7) if env.is_valid(a)])
+            validMoves = [a for a in range(7) if env.is_valid(a)]
+            entry = ([0] * 8, [0] * 7, validMoves, validMoves.copy())
             self._states[env.stateKey] = entry
         return entry
 
--- src/dev/connect4/validation.py
+++ src/dev/connect4/validation.py
@@ -2,14 +2,17 @@ import torch as T
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+import torch.multiprocessing as mp
+import copy
 from board import Connect4Board
 from board2dqn import getBestAction, createStateTensor
 from mcts import Connect4MCTS
 
+
 @T.no_grad()
-def _playValidationGame(model : nn.Module, qplayer : int, strength : int) -> Connect4Board:
+def _playValidationGame(model : nn.Module, qplayer : int, mctsGames : int) -> Connect4Board:
     env = Connect4Board()
-    mcts = Connect4MCTS(strength)
+    mcts = Connect4MCTS(mctsGames)
     while not env.Finished:
         if qplayer == env.Player:
             state = createStateTensor(env)
@@ -20,42 +23,64 @@ def _playValidationGame(model : nn.Module, qplayer : int, strength : int) -> Con
         env.move(action)
     return env
 
+def _validationProcess(model : nn.Module, qplayer : int, games : int, mctsGames : int) -> tuple[int, int, int, int]:
+    wins = draws = losses = 0
+    gameKeys = set()
+    for _ in range(games):
+        board = _playValidationGame(model, qplayer, mctsGames)
+        gameKeys.add(board.gameKey)
+        if board.Winner == qplayer:
+            wins += 1
+        elif board.Winner != Connect4Board.EMPTY:
+            losses += 1
+        else:
+            draws += 1
+
+    return (qplayer, wins, draws, losses, gameKeys)
+
 @T.no_grad()
-def validate(model : nn.Module, gamesPerPlayer : int, strength : int) -> None:
-    print(f'Validation with MCTS at {strength} games for {gamesPerPlayer} games per player.')
+def validate(model : nn.Module, gamesPerPlayer : int, processesPerPlayer : int, mctsGames : int) -> None:
+    if processesPerPlayer < 1:
+        raise ValueError('At least one validation process per player is required.')
+    if gamesPerPlayer < 1:
+        raise ValueError('At least one validation game per player is required.')
+    if mctsGames < 1:
+        raise ValueError('At least one mcts game per move is required.')
+    
+    print(f'Validation with {gamesPerPlayer} games per player on {processesPerPlayer} processes each, MCTS with {mctsGames} games.')
 
     training = model.training
     model.eval()
 
-    wins = losses = draws = 0
-    games = set()
-    for _ in range(gamesPerPlayer):
-        board = _playValidationGame(model, Connect4Board.PLAYER1, strength)
-        gk = board.gameKey
-        games.add(gk)
-        if board.Winner == Connect4Board.PLAYER1:
-            wins += 1
-        elif board.Winner == Connect4Board.PLAYER2:
-            losses += 1
-        else:
-            draws += 1
-
-    print(f'Player 1: {wins} won, {losses} lost, {draws} draws -> {100*wins/gamesPerPlayer:.2f}%, div: {100*len(games)/gamesPerPlayer:.2f}%')
-
-    wins = losses = draws = 0
-    games = set()
-    for _ in range(gamesPerPlayer):
-        board = _playValidationGame(model, Connect4Board.PLAYER2, strength)
-        gk = board.gameKey
-        games.add(gk)
-        if board.Winner == Connect4Board.PLAYER2:
-            wins += 1
-        elif board.Winner == Connect4Board.PLAYER1:
-            losses += 1
-        else:
-            draws += 1
-
-    print(f'Player 2: {wins} won, {losses} lost, {draws} draws -> {100*wins/gamesPerPlayer:.2f}%, div: {100*len(games)/gamesPerPlayer:.2f}%')
+    gamesPerProcess = gamesPerPlayer // processesPerPlayer
+    arguments = [(copy.deepcopy(model), qplayer, gamesPerProcess, mctsGames) for _ in range(processesPerPlayer) for qplayer in [Connect4Board.PLAYER1, Connect4Board.PLAYER2]]
+    with mp.Pool(processes=len(arguments)) as pool:
+        results = pool.starmap(_validationProcess, arguments)
 
     if training:
         model.train()
+
+    wins1 = losses1 = draws1 = wins2 = losses2 = draws2 = 0
+    games1 = set()
+    games2 = set()
+
+    for (qplayer, wins, draws, losses, games) in results:
+        if qplayer == Connect4Board.PLAYER1:
+            games1 = games1.union(games)
+            wins1 += wins
+            losses1 += losses
+            draws1 += draws
+        else:
+            games2 = games2.union(games)
+            wins2 += wins
+            losses2 += losses
+            draws2 += draws
+
+    total1 = wins1+losses1+draws1
+    unique1 = len(games1)
+    total2 = wins2+losses2+draws2
+    unique2 = len(games2)
+
+    print(f'Player 1: {wins1} won, {losses1} lost, {draws1} draws -> {100*wins1/total1:.2f}%, div: {100*unique1/total1:.2f}%')
+    print(f'Player 2: {wins2} won, {losses2} lost, {draws2} draws -> {100*wins2/total2:.2f}%, div: {100*unique2/total2:.2f}%')
+
