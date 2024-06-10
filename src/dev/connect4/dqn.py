57b4edb4c55109dd0a07c0eaaf9b296d7920957e
René Vogt <rene.vogt@gmx.de>
2024-06-10 14:57:42 +0200

    experimenting with architecture and hyper parameters

--- src/dev/connect4/dqn.py
+++ src/dev/connect4/dqn.py
@@ -13,21 +13,25 @@ class Connect4Dqn(nn.Module):
 
         # input: [B,3,6,7]
 
-        self.conv = nn.Conv2d(3, 64, kernel_size=4, stride=1, padding=1)    # [B,64,5,6]
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)       # [B,64,6,7]
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)     # [B,128,6,7]
 
-        self.flatten = nn.Flatten(start_dim=1) # [B, 1920]
+        self.flatten = nn.Flatten(start_dim=1) # [B, 5376]
 
-        self.fc1 = nn.Linear(1920, 960)
-        self.fc2 = nn.Linear(960, 240)
-        self.fc3 = nn.Linear(240, 7)
+        self.fc1 = nn.Linear(5376, 2688)
+        self.fc2 = nn.Linear(2688, 672)
+        self.fc3 = nn.Linear(672, 84)
+        self.fc4 = nn.Linear(84, 7)
 
         self.loss = nn.MSELoss()
         self.optimizer = o.SGD(self.parameters(), lr)
 
     def forward(self, x):
-        x = F.relu(self.conv(x))
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
         x = self.flatten(x)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
         return x
